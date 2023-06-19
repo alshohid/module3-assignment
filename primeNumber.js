7dfbd1e020616b21222b7dff86829a8796bf9cc1
alshohid <alshohid1997@gmail.com>
2023-06-19 21:03:59 +0600

    adding files

---
+++ primeNumber.js
@@ -0,0 +1,10 @@
+//03  (a)................................
+const isPrimeNumber =(num)=>{
+     if(num %2===0 && num >0)return true;
+     return false;
+}
+const number=-2;
+ const result = isPrimeNumber(number);
+ console.log(result);
+
+ // if num is positive and num%2 is equal to zero then it return true otherwise false .
